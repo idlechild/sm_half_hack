478aa27b36b6a2e2e39c10c40eab0a09f67181db
idle <idlechild123@gmail.com>
2023-11-21 00:01:05 -0600

    Replace samus data text with space time text

--- resources/spacetime.asm
+++ resources/spacetime.asm
@@ -1,4 +1,12 @@
 
+org $81B40A
+hook_samus_data_text:
+    ;      S      P      A      C      E      _      T      I      M      E
+    dw $202B, $200D, $200A, $200C, $200E, $200F, $202C, $2022, $2026, $200E, $FFFE
+    dw $203B, $2038, $201A, $201C, $201E, $200F, $2011, $2011, $2036, $201E, $FFFF
+
+
+
 org $90ACF6
 hook_load_beam_palette_external:
     JSR original_load_beam_palette

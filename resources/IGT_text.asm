17f567f02ce24eb2b3b0ea7629e6fa045c90f2f3
idle <idlechild123@gmail.com>
2025-06-16 00:49:41 -0500

    No Animal Tech

--- resources/IGT_text.asm
+++ resources/IGT_text.asm
@@ -26,22 +26,72 @@ warnpc $81FB00
 org $8B97D2
     JSL AddSpritemapToOAMWithDataPointer
 
+org $8BEB91
+IGTTheOperationWasDefinition:
+
 org $8BEECD
 IGTCompletedSuccessfullyDefinition:
 
 org $8BEEFD
 IGTClearTimeDefinition:
 
-org $8BF3B1
-    LDY !ram_IGT_completed_text
-
-org $8BF3CF
-    LDY !ram_IGT_clear_time_text
-
 
 
 org $8BFA00
 print pc, " IGT_text bank $8B start"
+
+IGTNoAnimalTechniquesDefinition:
+    dw $F02B, $F3B9, #IGTNoAnimalTechniquesInstructions
+
+IGTNoAnimalTechniquesInstructions:
+    dw #$0007, #IGTText_N
+    dw #$000C, #IGTText_No
+    dw #$0007, #IGTText_NoA
+    dw #$0007, #IGTText_NoAn
+    dw #$0007, #IGTText_NoAni
+    dw #$0007, #IGTText_NoAnim
+    dw #$0007, #IGTText_NoAnima
+    dw #$000C, #IGTText_NoAnimal
+    dw #$0007, #IGTText_NoAnimalT
+    dw #$0007, #IGTText_NoAnimalTe
+    dw #$0007, #IGTText_NoAnimalTec
+    dw #$0007, #IGTText_NoAnimalTech
+    dw #$0007, #IGTText_NoAnimalTechn
+    dw #$0007, #IGTText_NoAnimalTechni
+    dw #$0007, #IGTText_NoAnimalTechniq
+    dw #$0007, #IGTText_NoAnimalTechniqu
+    dw #$0007, #IGTText_NoAnimalTechnique
+    dw #$000C, #IGTText_NoAnimalTechniques
+    dw $F3B0
+  .loop
+    dw #$000F, #IGTText_NoAnimalTechniques
+    dw $94BC, #.loop
+
+IGTWereUsedInThisRunDefinition:
+    dw $F02B, $F3B9, #IGTWereUsedInThisRunInstructions
+
+IGTWereUsedInThisRunInstructions:
+    dw #$0008, #IGTText_W
+    dw #$0008, #IGTText_We
+    dw #$0008, #IGTText_Wer
+    dw #$0011, #IGTText_Were
+    dw #$0008, #IGTText_WereU
+    dw #$0008, #IGTText_WereUs
+    dw #$0008, #IGTText_WereUse
+    dw #$0011, #IGTText_WereUsed
+    dw #$0008, #IGTText_WereUsedI
+    dw #$0011, #IGTText_WereUsedIn
+    dw #$0008, #IGTText_WereUsedInT
+    dw #$0008, #IGTText_WereUsedInTh
+    dw #$0008, #IGTText_WereUsedInThi
+    dw #$0011, #IGTText_WereUsedInThis
+    dw #$0008, #IGTText_WereUsedInThisR
+    dw #$0008, #IGTText_WereUsedInThisRu
+    dw #$0012, #IGTText_WereUsedInThisRun
+    dw $F3CE
+  .loop
+    dw #$0008, #IGTText_WereUsedInThisRun
+    dw $94BC, #.loop
 
 IGTCompletedWithSpinLockDefinition:
     dw $F02B, $F3B9, #IGTCompletedWithSpinLockInstructions
@@ -204,6 +254,128 @@ IGTTrueCompletionInstructions:
 print pc, " IGT_text bank $8B end"
 
 
+
+org $8CA69D
+    dw $AB61, #$0002    ; Point to 'T'
+IGTText_N:
+    dw #IGTTextData_N, #$0002
+IGTText_No:
+    dw #IGTTextData_No, #$0004
+warnpc $8CA6A9
+
+org $8CA6A9
+    dw $AB57, #$0004    ; Point to 'Th'
+IGTText_NoA:
+    dw #IGTTextData_NoA, #$0006
+IGTText_NoAn:
+    dw #IGTTextData_NoAn, #$0008
+IGTText_NoAni:
+    dw #IGTTextData_NoAni, #$000A
+IGTText_NoAnim:
+    dw #IGTTextData_NoAnim, #$000C
+warnpc $8CA6BF
+
+org $8CA6BF
+    dw $AB4D, #$0006    ; Point to 'The'
+IGTText_NoAnima:
+    dw #IGTTextData_NoAnima, #$000E
+IGTText_NoAnimal:
+    dw #IGTTextData_NoAnimal, #$0010
+IGTText_NoAnimalT:
+    dw #IGTTextData_NoAnimalT, #$0012
+IGTText_NoAnimalTe:
+    dw #IGTTextData_NoAnimalTe, #$0014
+IGTText_NoAnimalTec:
+    dw #IGTTextData_NoAnimalTec, #$0016
+IGTText_NoAnimalTech:
+    dw #IGTTextData_NoAnimalTech, #$0018
+IGTText_NoAnimalTechn:
+    dw #IGTTextData_NoAnimalTechn, #$001A
+warnpc $8CA6DF
+
+org $8CA6DF
+    dw $AB43, #$0008    ; Point to 'The O'
+IGTText_NoAnimalTechni:
+    dw #IGTTextData_NoAnimalTechni, #$001C
+IGTText_NoAnimalTechniq:
+    dw #IGTTextData_NoAnimalTechniq, #$001E
+IGTText_NoAnimalTechniqu:
+    dw #IGTTextData_NoAnimalTechniqu, #$0020
+IGTText_NoAnimalTechnique:
+    dw #IGTTextData_NoAnimalTechnique, #$0022
+IGTText_W:
+    dw #IGTTextData_W, #$0002
+IGTText_We:
+    dw #IGTTextData_We, #$0004
+IGTText_Wer:
+    dw #IGTTextData_Wer, #$0006
+IGTText_Were:
+    dw #IGTTextData_Were, #$0008
+warnpc $8CA709
+
+org $8CA709
+    dw $AB39, #$000A    ; Point to 'The Op'
+IGTText_WereU:
+    dw #IGTTextData_WereU, #$000A
+IGTText_WereUs:
+    dw #IGTTextData_WereUs, #$000C
+IGTText_WereUse:
+    dw #IGTTextData_WereUse, #$000E
+IGTText_WereUsed:
+    dw #IGTTextData_WereUsed, #$0010
+IGTText_WereUsedI:
+    dw #IGTTextData_WereUsedI, #$0012
+IGTText_WereUsedIn:
+    dw #IGTTextData_WereUsedIn, #$0014
+IGTText_WereUsedInT:
+    dw #IGTTextData_WereUsedInT, #$0016
+IGTText_WereUsedInTh:
+    dw #IGTTextData_WereUsedInTh, #$0018
+IGTText_WereUsedInThi:
+    dw #IGTTextData_WereUsedInThi, #$001A
+IGTText_WereUsedInThis:
+    dw #IGTTextData_WereUsedInThis, #$001C
+IGTText_WereUsedInThisR:
+    dw #IGTTextData_WereUsedInThisR, #$001E
+IGTText_WereUsedInThisRu:
+    dw #IGTTextData_WereUsedInThisRu, #$0020
+warnpc $8CA73D
+
+org $8CA73D
+    dw $AB2F, #$000C    ; Point to 'The Ope'
+warnpc $8CA77B
+
+org $8CA77B
+    dw $AB25, #$000E    ; Point to 'The Oper'
+warnpc $8CA7C3
+
+org $8CA7C3
+    dw $AB1B, #$0010    ; Point to 'The Opera'
+warnpc $8CA815
+
+org $8CA815
+    dw $AB11, #$0012    ; Point to 'The Operat'
+warnpc $8CA871
+
+org $8CA871
+    dw $AB07, #$0014    ; Point to 'The Operati'
+warnpc $8CA8D7
+
+org $8CA8D7
+    dw $AAFD, #$0016    ; Point to 'The Operatio'
+warnpc $8CA947
+
+org $8CA947
+    dw $AAF3, #$0018    ; Point to 'The Operation'
+warnpc $8CA9C1
+
+org $8CA9C1
+    dw $AAE9, #$001A    ; Point to 'The Operation W'
+warnpc $8CAA45
+
+org $8CAA45
+    dw $AADF, #$001C    ; Point to 'The Operation Wa'
+warnpc $8CAAD3
 
 org $8CAB6B
     dw $B491, #$0002    ; Point to 'C'
@@ -465,6 +637,38 @@ org $8CB2FD
     dw $B3D3, #$0028    ; Point to 'Completed Successfull'
 warnpc $8CB3C7
 
+org $8CB49B
+    dw $B665, #$0002    ; Point to 'C'
+warnpc $8CB4A7
+
+org $8CB4A7
+    dw $B65B, #$0004    ; Point to 'Cl'
+warnpc $8CB4BD
+
+org $8CB4BD
+    dw $B651, #$0006    ; Point to 'Cle'
+warnpc $8CB4DD
+
+org $8CB4DD
+    dw $B647, #$0008    ; Point to 'Clea'
+warnpc $8CB507
+
+org $8CB507
+    dw $B63D, #$000A    ; Point to 'Clear'
+warnpc $8CB53B
+
+org $8CB53B
+    dw $B633, #$000C    ; Point to 'Clear T'
+warnpc $8CB579
+
+org $8CB579
+    dw $B629, #$000E    ; Point to 'Clear Ti'
+warnpc $8CB5C1
+
+org $8CB5C1
+    dw $B61F, #$0010    ; Point to 'Clear Tim'
+warnpc $8CB613
+
 
 
 org $8CF400
@@ -473,221 +677,301 @@ print pc, " IGT_text bank $8C start"
 macro IGTTextChar(xPos, yPos, cByte)
     dw <xPos>
     db <yPos>+$08
-    dw $3110+<cByte>
+table ../resources/IGTLowerChar.tbl
+    db "<cByte>"
+    db $31
     dw <xPos>
     db <yPos>
-    dw $3100+<cByte>
+table ../resources/IGTUpperChar.tbl
+    db "<cByte>"
+    db $31
 endmacro
+
+IGTText_NoAnimalTechniques:
+    dw #$0024
+IGTTextData_NoAnimalTechniques:
+    %IGTTextChar($48, $F8, "S")
+IGTTextData_NoAnimalTechnique:
+    %IGTTextChar($40, $F8, "E")
+IGTTextData_NoAnimalTechniqu:
+    %IGTTextChar($38, $F8, "U")
+IGTTextData_NoAnimalTechniq:
+    %IGTTextChar($30, $F8, "Q")
+IGTTextData_NoAnimalTechni:
+    %IGTTextChar($28, $F8, "I")
+IGTTextData_NoAnimalTechn:
+    %IGTTextChar($20, $F8, "N")
+IGTTextData_NoAnimalTech:
+    %IGTTextChar($18, $F8, "H")
+IGTTextData_NoAnimalTec:
+    %IGTTextChar($10, $F8, "C")
+IGTTextData_NoAnimalTe:
+    %IGTTextChar($08, $F8, "E")
+IGTTextData_NoAnimalT:
+    %IGTTextChar($00, $F8, "T")
+IGTTextData_NoAnimal:
+    %IGTTextChar($1F0, $F8, "L")
+IGTTextData_NoAnima:
+    %IGTTextChar($1E8, $F8, "A")
+IGTTextData_NoAnim:
+    %IGTTextChar($1E0, $F8, "M")
+IGTTextData_NoAni:
+    %IGTTextChar($1D8, $F8, "I")
+IGTTextData_NoAn:
+    %IGTTextChar($1D0, $F8, "N")
+IGTTextData_NoA:
+    %IGTTextChar($1C8, $F8, "A")
+IGTTextData_No:
+    %IGTTextChar($1B8, $F8, "O")
+IGTTextData_N:
+    %IGTTextChar($1B0, $F8, "N")
+
+IGTText_WereUsedInThisRun:
+    dw #$0022
+IGTTextData_WereUsedInThisRun:
+    %IGTTextChar($50, $10, "N")
+IGTTextData_WereUsedInThisRu:
+    %IGTTextChar($48, $10, "U")
+IGTTextData_WereUsedInThisR:
+    %IGTTextChar($40, $10, "R")
+IGTTextData_WereUsedInThis:
+    %IGTTextChar($30, $10, "S")
+IGTTextData_WereUsedInThi:
+    %IGTTextChar($28, $10, "I")
+IGTTextData_WereUsedInTh:
+    %IGTTextChar($20, $10, "H")
+IGTTextData_WereUsedInT:
+    %IGTTextChar($18, $10, "T")
+IGTTextData_WereUsedIn:
+    %IGTTextChar($08, $10, "N")
+IGTTextData_WereUsedI:
+    %IGTTextChar($00, $10, "I")
+IGTTextData_WereUsed:
+    %IGTTextChar($1F0, $10, "D")
+IGTTextData_WereUse:
+    %IGTTextChar($1E8, $10, "E")
+IGTTextData_WereUs:
+    %IGTTextChar($1E0, $10, "S")
+IGTTextData_WereU:
+    %IGTTextChar($1D8, $10, "U")
+IGTTextData_Were:
+    %IGTTextChar($1C8, $10, "E")
+IGTTextData_Wer:
+    %IGTTextChar($1C0, $10, "R")
+IGTTextData_We:
+    %IGTTextChar($1B8, $10, "E")
+IGTTextData_W:
+    %IGTTextChar($1B0, $10, "W")
 
 IGTText_CompletedWithSpinLock:
     dw #$002A
 IGTTextData_CompletedWithSpinLock:
-    %IGTTextChar($58, $10, $2A)
+    %IGTTextChar($58, $10, "K")
 IGTTextData_CompletedWithSpinLoc:
-    %IGTTextChar($50, $10, $22)
+    %IGTTextChar($50, $10, "C")
 IGTTextData_CompletedWithSpinLo:
-    %IGTTextChar($48, $10, $2E)
+    %IGTTextChar($48, $10, "O")
 IGTTextData_CompletedWithSpinL:
-    %IGTTextChar($40, $10, $2B)
+    %IGTTextChar($40, $10, "L")
 IGTTextData_CompletedWithSpin:
-    %IGTTextChar($30, $10, $2D)
+    %IGTTextChar($30, $10, "N")
 IGTTextData_CompletedWithSpi:
-    %IGTTextChar($28, $10, $28)
+    %IGTTextChar($28, $10, "I")
 IGTTextData_CompletedWithSp:
-    %IGTTextChar($20, $10, $2F)
+    %IGTTextChar($20, $10, "P")
 IGTTextData_CompletedWithS:
-    %IGTTextChar($18, $10, $42)
+    %IGTTextChar($18, $10, "S")
 IGTTextData_CompletedWith:
-    %IGTTextChar($08, $10, $27)
+    %IGTTextChar($08, $10, "H")
 IGTTextData_CompletedWit:
-    %IGTTextChar($00, $10, $43)
+    %IGTTextChar($00, $10, "T")
 IGTTextData_CompletedWi:
-    %IGTTextChar($1F8, $10, $28)
+    %IGTTextChar($1F8, $10, "I")
 IGTTextData_CompletedW:
-    %IGTTextChar($1F0, $10, $46)
+    %IGTTextChar($1F0, $10, "W")
 IGTTextData_Completed:
-    %IGTTextChar($1E0, $10, $23)
+    %IGTTextChar($1E0, $10, "D")
 IGTTextData_Complete:
-    %IGTTextChar($1D8, $10, $24)
+    %IGTTextChar($1D8, $10, "E")
 IGTTextData_Complet:
-    %IGTTextChar($1D0, $10, $43)
+    %IGTTextChar($1D0, $10, "T")
 IGTTextData_Comple:
-    %IGTTextChar($1C8, $10, $24)
+    %IGTTextChar($1C8, $10, "E")
 IGTTextData_Compl:
-    %IGTTextChar($1C0, $10, $2B)
+    %IGTTextChar($1C0, $10, "L")
 IGTTextData_Comp:
-    %IGTTextChar($1B8, $10, $2F)
+    %IGTTextChar($1B8, $10, "P")
 IGTTextData_Com:
-    %IGTTextChar($1B0, $10, $2C)
+    %IGTTextChar($1B0, $10, "M")
 IGTTextData_Co:
-    %IGTTextChar($1A8, $10, $2E)
+    %IGTTextChar($1A8, $10, "O")
 IGTTextData_C:
-    %IGTTextChar($1A0, $10, $22)
+    %IGTTextChar($1A0, $10, "C")
 
 IGTText_GTMaxCompletion:
     dw #$001E
 IGTTextData_GTMaxCompletion:
-    %IGTTextChar($38, $10, $2D)
+    %IGTTextChar($38, $10, "N")
 IGTTextData_GTMaxCompletio:
-    %IGTTextChar($30, $10, $2E)
+    %IGTTextChar($30, $10, "O")
 IGTTextData_GTMaxCompleti:
-    %IGTTextChar($28, $10, $28)
+    %IGTTextChar($28, $10, "I")
 IGTTextData_GTMaxComplet:
-    %IGTTextChar($20, $10, $43)
+    %IGTTextChar($20, $10, "T")
 IGTTextData_GTMaxComple:
-    %IGTTextChar($18, $10, $24)
+    %IGTTextChar($18, $10, "E")
 IGTTextData_GTMaxCompl:
-    %IGTTextChar($10, $10, $2B)
+    %IGTTextChar($10, $10, "L")
 IGTTextData_GTMaxComp:
-    %IGTTextChar($08, $10, $2F)
+    %IGTTextChar($08, $10, "P")
 IGTTextData_GTMaxCom:
-    %IGTTextChar($00, $10, $2C)
+    %IGTTextChar($00, $10, "M")
 IGTTextData_GTMaxCo:
-    %IGTTextChar($1F8, $10, $2E)
+    %IGTTextChar($1F8, $10, "O")
 IGTTextData_GTMaxC:
-    %IGTTextChar($1F0, $10, $22)
+    %IGTTextChar($1F0, $10, "C")
 IGTTextData_GTMax:
-    %IGTTextChar($1E0, $10, $47)
+    %IGTTextChar($1E0, $10, "X")
 IGTTextData_GTMa:
-    %IGTTextChar($1D8, $10, $20)
+    %IGTTextChar($1D8, $10, "A")
 IGTTextData_GTM:
-    %IGTTextChar($1D0, $10, $2C)
+    %IGTTextChar($1D0, $10, "M")
 IGTTextData_GT:
-    %IGTTextChar($1C0, $10, $43)
+    %IGTTextChar($1C0, $10, "T")
 IGTTextData_G:
-    %IGTTextChar($1B8, $10, $26)
+    %IGTTextChar($1B8, $10, "G")
 
 IGTText_MapCompletion:
     dw #$001A
 IGTTextData_MapCompletion:
-    %IGTTextChar($30, $10, $2D)
+    %IGTTextChar($30, $10, "N")
 IGTTextData_MapCompletio:
-    %IGTTextChar($28, $10, $2E)
+    %IGTTextChar($28, $10, "O")
 IGTTextData_MapCompleti:
-    %IGTTextChar($20, $10, $28)
+    %IGTTextChar($20, $10, "I")
 IGTTextData_MapComplet:
-    %IGTTextChar($18, $10, $43)
+    %IGTTextChar($18, $10, "T")
 IGTTextData_MapComple:
-    %IGTTextChar($10, $10, $24)
+    %IGTTextChar($10, $10, "E")
 IGTTextData_MapCompl:
-    %IGTTextChar($08, $10, $2B)
+    %IGTTextChar($08, $10, "L")
 IGTTextData_MapComp:
-    %IGTTextChar($00, $10, $2F)
+    %IGTTextChar($00, $10, "P")
 IGTTextData_MapCom:
-    %IGTTextChar($1F8, $10, $2C)
+    %IGTTextChar($1F8, $10, "M")
 IGTTextData_MapCo:
-    %IGTTextChar($1F0, $10, $2E)
+    %IGTTextChar($1F0, $10, "O")
 IGTTextData_MapC:
-    %IGTTextChar($1E8, $10, $22)
+    %IGTTextChar($1E8, $10, "C")
 IGTTextData_Map:
-    %IGTTextChar($1D8, $10, $2F)
+    %IGTTextChar($1D8, $10, "P")
 IGTTextData_Ma:
-    %IGTTextChar($1D0, $10, $20)
+    %IGTTextChar($1D0, $10, "A")
 IGTTextData_M:
-    %IGTTextChar($1C8, $10, $2C)
+    %IGTTextChar($1C8, $10, "M")
 
 IGTText_ReloadCount:
     dw #$0016
 IGTTextData_ReloadCount:
-    %IGTTextChar($10, $F8, $43)
+    %IGTTextChar($10, $F8, "T")
 IGTTextData_ReloadCoun:
-    %IGTTextChar($08, $F8, $2D)
+    %IGTTextChar($08, $F8, "N")
 IGTTextData_ReloadCou:
-    %IGTTextChar($00, $F8, $44)
+    %IGTTextChar($00, $F8, "U")
 IGTTextData_ReloadCo:
-    %IGTTextChar($1F8, $F8, $2E)
+    %IGTTextChar($1F8, $F8, "O")
 IGTTextData_ReloadC:
-    %IGTTextChar($1F0, $F8, $22)
+    %IGTTextChar($1F0, $F8, "C")
 IGTTextData_Reload:
-    %IGTTextChar($1E0, $F8, $23)
+    %IGTTextChar($1E0, $F8, "D")
 IGTTextData_Reloa:
-    %IGTTextChar($1D8, $F8, $20)
+    %IGTTextChar($1D8, $F8, "A")
 IGTTextData_Relo:
-    %IGTTextChar($1D0, $F8, $2E)
+    %IGTTextChar($1D0, $F8, "O")
 IGTTextData_Rel:
-    %IGTTextChar($1C8, $F8, $2B)
+    %IGTTextChar($1C8, $F8, "L")
 IGTTextData_Re:
-    %IGTTextChar($1C0, $F8, $24)
+    %IGTTextChar($1C0, $F8, "E")
 IGTTextData_R:
-    %IGTTextChar($1B8, $F8, $41)
+    %IGTTextChar($1B8, $F8, "R")
 
 IGTText_SuitlessTrueCompletion:
     dw #$002C
 IGTTextData_SuitlessTrueCompletion:
-    %IGTTextChar($58, $10, $2D)
+    %IGTTextChar($58, $10, "N")
 IGTTextData_SuitlessTrueCompletio:
-    %IGTTextChar($50, $10, $2E)
+    %IGTTextChar($50, $10, "O")
 IGTTextData_SuitlessTrueCompleti:
-    %IGTTextChar($48, $10, $28)
+    %IGTTextChar($48, $10, "I")
 IGTTextData_SuitlessTrueComplet:
-    %IGTTextChar($40, $10, $43)
+    %IGTTextChar($40, $10, "T")
 IGTTextData_SuitlessTrueComple:
-    %IGTTextChar($38, $10, $24)
+    %IGTTextChar($38, $10, "E")
 IGTTextData_SuitlessTrueCompl:
-    %IGTTextChar($30, $10, $2B)
+    %IGTTextChar($30, $10, "L")
 IGTTextData_SuitlessTrueComp:
-    %IGTTextChar($28, $10, $2F)
+    %IGTTextChar($28, $10, "P")
 IGTTextData_SuitlessTrueCom:
-    %IGTTextChar($20, $10, $2C)
+    %IGTTextChar($20, $10, "M")
 IGTTextData_SuitlessTrueCo:
-    %IGTTextChar($18, $10, $2E)
+    %IGTTextChar($18, $10, "O")
 IGTTextData_SuitlessTrueC:
-    %IGTTextChar($10, $10, $22)
+    %IGTTextChar($10, $10, "C")
 IGTTextData_SuitlessTrue:
-    %IGTTextChar($00, $10, $24)
+    %IGTTextChar($00, $10, "E")
 IGTTextData_SuitlessTru:
-    %IGTTextChar($1F8, $10, $44)
+    %IGTTextChar($1F8, $10, "U")
 IGTTextData_SuitlessTr:
-    %IGTTextChar($1F0, $10, $41)
+    %IGTTextChar($1F0, $10, "R")
 IGTTextData_SuitlessT:
-    %IGTTextChar($1E8, $10, $43)
+    %IGTTextChar($1E8, $10, "T")
 IGTTextData_Suitless:
-    %IGTTextChar($1D8, $10, $42)
+    %IGTTextChar($1D8, $10, "S")
 IGTTextData_Suitles:
-    %IGTTextChar($1D0, $10, $42)
+    %IGTTextChar($1D0, $10, "S")
 IGTTextData_Suitle:
-    %IGTTextChar($1C8, $10, $24)
+    %IGTTextChar($1C8, $10, "E")
 IGTTextData_Suitl:
-    %IGTTextChar($1C0, $10, $2B)
+    %IGTTextChar($1C0, $10, "L")
 IGTTextData_Suit:
-    %IGTTextChar($1B8, $10, $43)
+    %IGTTextChar($1B8, $10, "T")
 IGTTextData_Sui:
-    %IGTTextChar($1B0, $10, $28)
+    %IGTTextChar($1B0, $10, "I")
 IGTTextData_Su:
-    %IGTTextChar($1A8, $10, $44)
+    %IGTTextChar($1A8, $10, "U")
 IGTTextData_S:
-    %IGTTextChar($1A0, $10, $42)
+    %IGTTextChar($1A0, $10, "S")
 
 IGTText_TrueCompletion:
     dw #$001C
 IGTTextData_TrueCompletion:
-    %IGTTextChar($30, $10, $2D)
+    %IGTTextChar($30, $10, "N")
 IGTTextData_TrueCompletio:
-    %IGTTextChar($28, $10, $2E)
+    %IGTTextChar($28, $10, "O")
 IGTTextData_TrueCompleti:
-    %IGTTextChar($20, $10, $28)
+    %IGTTextChar($20, $10, "I")
 IGTTextData_TrueComplet:
-    %IGTTextChar($18, $10, $43)
+    %IGTTextChar($18, $10, "T")
 IGTTextData_TrueComple:
-    %IGTTextChar($10, $10, $24)
+    %IGTTextChar($10, $10, "E")
 IGTTextData_TrueCompl:
-    %IGTTextChar($08, $10, $2B)
+    %IGTTextChar($08, $10, "L")
 IGTTextData_TrueComp:
-    %IGTTextChar($00, $10, $2F)
+    %IGTTextChar($00, $10, "P")
 IGTTextData_TrueCom:
-    %IGTTextChar($1F8, $10, $2C)
+    %IGTTextChar($1F8, $10, "M")
 IGTTextData_TrueCo:
-    %IGTTextChar($1F0, $10, $2E)
+    %IGTTextChar($1F0, $10, "O")
 IGTTextData_TrueC:
-    %IGTTextChar($1E8, $10, $22)
+    %IGTTextChar($1E8, $10, "C")
 IGTTextData_True:
-    %IGTTextChar($1D8, $10, $24)
+    %IGTTextChar($1D8, $10, "E")
 IGTTextData_Tru:
-    %IGTTextChar($1D0, $10, $44)
+    %IGTTextChar($1D0, $10, "U")
 IGTTextData_Tr:
-    %IGTTextChar($1C8, $10, $41)
+    %IGTTextChar($1C8, $10, "R")
 IGTTextData_T:
-    %IGTTextChar($1C0, $10, $43)
+    %IGTTextChar($1C0, $10, "T")
 
 print pc, " IGT_text bank $8C end"
 
---
+++ src/no_animal_tech.asm
@@ -0,0 +1,218 @@
+lorom
+
+incsrc ../resources/IGT_text.asm
+
+
+
+org $858086
+    JMP override_fanfare_selection
+return_from_override_fanfare_selection:
+
+org $85810B
+    LDA $09C4 : BMI .override
+    LDA $05F9
+    RTS
+  .override
+    LDA #$0002
+    RTS
+warnpc $858119
+
+org $858412
+    ; Relocated this table to make room for more message box types
+    LDA SpecialButtonTilemapOffsets,X
+
+org $858749
+original_button_tilemap_offset_table:
+    ; Expand message definitions
+    dw #$8436, #$8289, SaveTooLateText
+    ; Currently room for seven more types
+    ; The last one must remain reserved
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+    dw #$8436, #$8289, EndFanfareText
+
+
+
+; Message text must be listed in order
+org $859643
+table ../resources/HUDfont.tbl
+SaveTooLateText:
+    dw #$000E, #$000E, #$000E, #$000E, #$000E, #$000E
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|'N'
+    dw #$2800|'O'
+    dw #$2800|'T'
+    dw #$2800|'H'
+    dw #$2800|'I'
+    dw #$2800|'N'
+    dw #$2800|'G'
+    dw #$2800|' '
+    dw #$2800|'C'
+    dw #$2800|'A'
+    dw #$2800|'N'
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$000E, #$000E, #$000E, #$000E, #$000E, #$000E, #$000E
+    dw #$000E, #$000E, #$000E, #$000E, #$000E, #$000E
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|'S'
+    dw #$2800|'A'
+    dw #$2800|'V'
+    dw #$2800|'E'
+    dw #$2800|' '
+    dw #$2800|'Y'
+    dw #$2800|'O'
+    dw #$2800|'U'
+    dw #$2800|' '
+    dw #$2800|'N'
+    dw #$2800|'O'
+    dw #$2800|'W'
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$2800|' '
+    dw #$000E, #$000E, #$000E, #$000E, #$000E, #$000E, #$000E
+
+EndFanfareText:
+
+; Recreate $8749: Special button tilemap offsets
+SpecialButtonTilemapOffsets:
+    dw #$0000  ; 1: Energy tank
+    dw #$012A  ; 2: Missile
+    dw #$012A  ; 3: Super missile
+    dw #$012C  ; 4: Power bomb
+    dw #$012C  ; 5: Grappling beam
+    dw #$012C  ; 6: X-ray scope
+    dw #$0000  ; 7: Varia suit
+    dw #$0000  ; 8: Spring ball
+    dw #$0000  ; 9: Morphing ball
+    dw #$0000  ; Ah: Screw attack
+    dw #$0000  ; Bh: Hi-jump boots
+    dw #$0000  ; Ch: Space jump
+    dw #$0120  ; Dh: Speed booster
+    dw #$0000  ; Eh: Charge beam
+    dw #$0000  ; Fh: Ice beam
+    dw #$0000  ; 10h: Wave beam
+    dw #$0000  ; 11h: Spazer
+    dw #$0000  ; 12h: Plasma beam
+    dw #$012A  ; 13h: Bomb
+    dw #$0000  ; 14h: Map data access completed
+    dw #$0000  ; 15h: Energy recharge completed
+    dw #$0000  ; 16h: Missile reload completed
+    dw #$0000  ; 17h: Would you like to save?
+    dw #$0000  ; 18h: Save completed
+    dw #$0000  ; 19h: Reserve tank
+    dw #$0000  ; 1Ah: Gravity suit
+    dw #$0000  ; 1Bh: Terminator
+    dw #$000E  ; 1Ch: Would you like to save? (Used by gunship)
+    dw #$000E  ; 1Dh: Reserved (Used by gunship)
+    dw #$0000  ; 1Eh: Save too late
+    dw #$0000  ; 1Fh:
+    dw #$0000  ; 20h:
+    dw #$0000  ; 21h:
+    dw #$0000  ; 22h:
+    dw #$0000  ; 23h:
+    dw #$0000  ; 24h:
+    dw #$0000  ; 25h:
+    dw #$0000  ; 26h: Reserved
+
+override_fanfare_selection:
+{
+    STA $1C1F
+    LDA $09C4 : BMI .override
+    JMP return_from_override_fanfare_selection
+
+  .override
+    LDA #$001E : STA $1C1F
+    JMP return_from_override_fanfare_selection
+}
+
+
+
+org $8BDE28
+    LDY #IGTNoAnimalTechniquesDefinition
+
+org $8BF3B1
+    LDY #IGTWereUsedInThisRunDefinition
+
+
+
+org $908E75
+challenge_failed_shinespark:
+    LDA #$F000 : STA $09C2 : STA $09C4
+    ; Overwritten code
+    LDA #$0008
+    RTS
+warnpc $908EA9
+
+org $90D01B
+    JSR challenge_failed_shinespark
+
+
+
+org $91EAC7
+    ; Optimize three bytes
+    BEQ walljump_set_left_pose
+    LDA #$0083
+    BRA walljump_set_pose
+
+walljump_set_left_pose:
+    LDA #$0084
+
+walljump_set_pose:
+    STA $0A28
+    LDA #$0005 : STA $0A2E
+    JMP challenge_failed_walljump
+warnpc $91EADE
+
+org $91EC9D
+challenge_failed_walljump:
+    LDA #$F000 : STA $09C2 : STA $09C4
+    RTS
+warnpc $91ECB4
+
+
+
+org $AAC8CB
+    ; Use a different RTS so we can overwrite the original RTS
+    LDA #$C6AB
+
+org $AAC90A
+    ; Optimize one byte
+    BNE check_gt_code
+    JSR $C250
+    JML $88DD32
+
+check_gt_code:
+    JSR $C280
+    LDA $8B : CMP #$C0C0 : BNE return_gt_code
+
+    ; GT code not allowed if challenge failed
+    LDA $09C4 : BMI return_gt_code
+
+    ; Set GT code, optimize three bytes
+    LDA #$02BC : STA $09C2 : STA $09C4
+    LDA #$012C : STA $09D4 : STA $09D6
+    LDA #$0064 : STA $09C6 : STA $09C8
+    LDA #$0014 : STA $09CA : STA $09CC
+    STA $09CE : STA $09D0
+    LDA #$F377 : STA $09A2 : STA $09A4
+    LDA #$100F : STA $09A6 : STA $09A8
+
+return_gt_code:
+    ; Overwrite RTS that is no longer used
+    RTL
+warnpc $AAC95F
+

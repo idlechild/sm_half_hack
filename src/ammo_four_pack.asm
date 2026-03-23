d5a9b9c48cc5073bf90e3669a0abb40abc0e223a
idle <idlechild123@gmail.com>
2026-03-22 21:38:15 -0500

    Add source code for ammo four pack

---
+++ src/ammo_four_pack.asm
@@ -0,0 +1,38 @@
+lorom
+
+; Visible missile pack ammo count
+org $84E0DD
+    dw #$0004
+
+; Visible super missile pack ammo count
+org $84E102
+    dw #$0004
+
+; Visible power bomb pack ammo count
+org $84E127
+    dw #$0004
+
+; Chozo orb missile pack ammo count
+org $84E4A6
+    dw #$0004
+
+; Chozo orb super missile pack ammo count
+org $84E4D8
+    dw #$0004
+
+; Chozo orb power bomb pack ammo count
+org $84E50A
+    dw #$0004
+
+; Hidden missile pack ammo count
+org $84E977
+    dw #$0004
+
+; Hidden super missile pack ammo count
+org $84E9AF
+    dw #$0004
+
+; Hidden power bomb pack ammo count
+org $84E9E7
+    dw #$0004
+
